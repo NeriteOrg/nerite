9f8dcadf9c808ce8b92fea2a1055f37b6ad5d242
MrDeadCe11 <mister.deadce11@gmail.com>
2025-04-22 03:14:20 -0500

    fixed rsETH

--- contracts/src/PriceFeeds/rsETHPriceFeed.sol
+++ contracts/src/PriceFeeds/rsETHPriceFeed.sol
@@ -7,7 +7,7 @@ import "./CompositePriceFeed.sol";
 contract rsETHPriceFeed is CompositePriceFeed {
 
     //BTC feed on arbitrum.
-    address public rsEthOracleAddress = 0xb0EA543f9F8d4B818550365d13F66Da747e1476A;
+    address public rsEthOracleAddress = 0x8fE61e9D74ab69cE9185F365dfc21FC168c4B56c;
     Oracle public rsEthOracle;
 
     constructor(address _owner, address _ethUsdOracleAddress, address _rsEthOracleAddress, uint256 _rsEthUsdStalenessThreshold)
@@ -32,35 +32,18 @@ contract rsETHPriceFeed is CompositePriceFeed {
         return (lastGoodPrice, false);
     }
 
-    function fetchRedemptionPrice() external returns (uint256, bool) {
-        // Use same price for redemption as all other ops in tBTC branch
+    function fetchRedemptionPrice(bool _isRedemption) external returns (uint256, bool) {
 
-        //get the price of normal rsETH in eth. 
-        int256 rsEthPriceInt;
-        (,rsEthPriceInt,,,) = AggregatorV3Interface(rsEthOracleAddress).latestRoundData();
-        uint256 rsEthPrice = uint256(rsEthPriceInt); // Convert from int256 to uint256
-        //compare tbtc price. 
-        (uint256 rsEthPrice, bool resultDown) = fetchPrice();
-        //if they are within 2% then there is no depeg. Use the higher one of the two to prevent value leak.
-        // Instead of comparing tbtcPrice/btcPrice with fractions, cross-multiply.
-        if (rsEthPrice * 100 <= rsEthPrice * 102 && rsEthPrice * 100 >= rsEthPrice * 98) {
-            //no depeg. return the higher one.
-            if( rsEthPrice > rsEthPrice) {
-                return (rsEthPrice, resultDown); 
-            } else {
-                return (btcPrice, resultDown);
-            }
+        if (_isRedemption && _withinDeviationThreshold(stEthUsdPrice, ethUsdPrice, STETH_USD_DEVIATION_THRESHOLD)) {
+            // If it's a redemption and within 1%, take the max of (STETH-USD, ETH-USD) to mitigate unwanted redemption arb and convert to WSTETH-USD
+            wstEthUsdPrice = LiquityMath._max(stEthUsdPrice, ethUsdPrice) * stEthPerWstEth / 1e18;
         } else {
-            //depeg is significant. 
-            return (tbtcPrice, resultDown);
+            // Otherwise, just calculate WSTETH-USD price: USD_per_WSTETH = USD_per_STETH * STETH_per_WSTETH
+            wstEthUsdPrice = stEthUsdPrice * stEthPerWstEth / 1e18;
         }
-    }
 
-    //  _fetchPricePrimary returns:
-    // - The price
-    // - A bool indicating whether a new oracle failure was detected in the call
-    function _fetchPricePrimary(bool /* _isRedemption */ ) internal virtual returns (uint256, bool) {
-        return _fetchPricePrimary();
+        // Use same price for redemption as all other ops in UNI branch
+        return fetchPrice();
     }
 
     function _fetchPricePrimary() internal returns (uint256, bool) {
@@ -73,9 +56,9 @@ contract rsETHPriceFeed is CompositePriceFeed {
         if (ethUsdOracleDown) return (_shutDownAndSwitchToLastGoodPrice(address(ethUsdOracle.aggregator)), true);
 
         // Calculate the canonical LST-USD price: USD_per_LST = USD_per_ETH * underlying_per_LST
-        uint256 lstUsdCanonicalPrice = _ethUsdPrice * lstRate / 1e18;
+        uint256 rsEthUsdPrice = ethUsdPrice * rsEthRate / 1e18;
 
-        lastGoodPrice = lstUsdCanonicalPrice;
-        return (lstUsdCanonicalPrice, false);
+        lastGoodPrice = rsEthUsdPrice;
+        return (rsEthUsdPrice, false);
     }
 }
